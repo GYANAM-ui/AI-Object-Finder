bed1a4f3ab9e1f67b030a44ea7199048d338b21b
GYANAM-DEVELOPER <74140267+GYANAM-ui@users.noreply.github.com>
2021-05-15 08:35:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status = " ";
+
 function setup() {
     canvas = createCanvas(400, 300);
     canvas.center();
@@ -7,4 +9,15 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 400, 300);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
 }
